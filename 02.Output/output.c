a68899d2c73c854da96073fb2aeac7bbfdd40254
Rezaul Karim <rezaul@orangetoolz.com>
2025-09-02 21:34:45 +0600

    doc added for setup and run

--- 02.Output/output.c
+++ 02.Output/output.c
@@ -3,22 +3,21 @@
 
 
 int main  () {
+    // Printing all of data
+    printf("my name is rejaul \n");
+    printf("I'm javascript developer,\t Curently i'm working on react native\n");
+    printf("10 + 5 = %d\n" ,10 + 5);
+    printf("10 - 5 = %d\n" ,10 - 5);
+    printf("10 * 5 = %d\n" ,10 * 5);
+    printf("10 / 5 = %d\n" ,10 / 5);
 
-// Printing all of data
-printf("my name is rejaul \n");
-printf("I'm javascript developer,\t Curently i'm working on react native\n");
-printf("10 + 5 = %d\n" ,10 + 5);
-printf("10 - 5 = %d\n" ,10 - 5);
-printf("10 * 5 = %d\n" ,10 * 5);
-printf("10 / 5 = %d\n" ,10 / 5);
+    int num1 = 30;
+    int num2 = 20;
 
-int num1 = 30;
-int num2 = 20;
+    printf("%d + %d = %d \n" , num1, num2,  num1 + num2  );
+    printf("%d - %d = %d \n" , num1, num2,  num1 - num2  );
+    printf("%d * %d = %d \n" , num1, num2,  num1 * num2  );
+    printf("%d / %d = %d \n" , num1, num2,  num1 / num2  );
 
-printf("%d + %d = %d \n" , num1, num2,  num1 + num2  );
-printf("%d - %d = %d \n" , num1, num2,  num1 - num2  );
-printf("%d * %d = %d \n" , num1, num2,  num1 * num2  );
-printf("%d / %d = %d \n" , num1, num2,  num1 / num2  );
-
-return 0;
+    return 0;
 }
---
+++ 03.variables/app.c
@@ -0,0 +1,5 @@
+int main() {
+    print1f("Hello World\n");
+    
+    return 0;
+}
